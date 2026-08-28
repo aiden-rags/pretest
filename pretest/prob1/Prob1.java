b638609839342ebe2669ced2fb3ac8b73a2df365
aiden-rags <aragsd100@uafs.edu>
2026-08-28 09:56:51 -0500

    finished toUpperCase

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -3,14 +3,32 @@ package prob1;
 public class Prob1 {
 
 	public static void main(String[] args) {
-		nums = new int[] {4, 7, 2, 9, 1};
-
-
-	}
-public static int findMaxDiffrence(int[]numbers) {
-	for(int=0; i<numbers.length; i++) {
+		int[] numbers = {};
+		int max;
 		
+		max = findMaxDifference(numbers);
+		System.out.printf("Max is %d\n", max);
+
 	}
-	return;
+	
+public static int findMaxDifference(int[] theArray) {
+//Finds the max difference between adjacent pairs in an array.
+	
+	int max = 0;
+	int index;
+	int diff;
+	
+	for(index=0; index<theArray.length-1; index++) {
+		diff = theArray[index] - theArray[index+1];
+		if(diff < 0) {
+			diff = diff * -1;
+		}
+		if(diff > max) {
+			max = diff;
+		}
+	}
+	
+	return max;	
+	}
 
 }
